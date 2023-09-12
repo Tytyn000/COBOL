969aa2f85bc8c30b38ff2100ac5d3e4e92e48456
Tytyn000 <tytyn@tutanota.com>
2023-09-12 22:30:12 +0200

    UpdateOfCalculator

--- Calculator.cbl
+++ Calculator.cbl
@@ -7,9 +7,9 @@
            DECIMAL-POINT IS COMMA.
        data division.
        working-storage section.
-       01 Number1 PIC S9(6)V9(6).
-       01 Number2 PIC S9(6)V9(6).
-       01 Resultat PIC S9(6)V9(6).
+       01 Number1 PIC S9(6)V9(15).
+       01 Number2 PIC S9(6)V9(15).
+       01 Resultat PIC S9(9)V9(15).
        01 PI PIC 9(1)V9(15) VALUE 3,141592653589793.
        01 Signe PIC X(1) VALUE SPACE.
        77 UserResponse PIC X(16).
@@ -17,9 +17,10 @@
                                                                              
        procedure division.
        DISPLAY "EN CAS DE VALEUR DECIMAL UTILSER , POUR LA SEPARATION"  
-       DISPLAY "Le . n'est pas un séparateur Entier,Decimal"
-       DISPLAY "Pour faire une racine carré écrivez SquareRoot"
-       DISPLAY "Sinon ignorez la ligne du dessus et appuyez sur ENTER"
+       DISPLAY 'Pour faire une racine carré "SquareRoot"'
+       DISPLAY 'Pour un calcul utilisant PI "PI"'
+       DISPLAY "La valeur actuelle de PI est de : " PI
+       DISPLAY "Sinon ignorez et appuyez sur ENTER"
        ACCEPT UserResponse.
        IF UserResponse = "SquareRoot" THEN
            DISPLAY "Entrez le chiffre"
@@ -31,12 +32,46 @@
            COMPUTE Resultat ROUNDED = FUNCTION SQRT (Number1)
            DISPLAY "Le résultat est : " Resultat
            STOP RUN
+       ELSE IF UserResponse = "PI" THEN
+           DISPLAY "Entrez le chiffre"
+           DISPLAY "Si le premier chiffre est PI écrivez 0"
+           ACCEPT Number1
+           IF Number1 = 0 THEN
+               MOVE PI TO Number1
+           END-IF
+           DISPLAY "Entrez le signe de l'opération(+, -, *, /)"
+           ACCEPT Signe
+           IF Signe = "+" THEN
+               COMPUTE Resultat ROUNDED = Number1 + PI
+               DISPLAY "Valeur possiblement non exact"  
+               DISPLAY "Le résultat est de : " Resultat        
+               STOP RUN      
+           ELSE IF Signe = "-" THEN
+               COMPUTE Resultat ROUNDED = Number1 - PI
+               DISPLAY "Valeur possiblement non exact"           
+               DISPLAY "Le résultat est de : " Resultat
+               STOP RUN 
+           ELSE IF Signe = "*" THEN
+               COMPUTE Resultat ROUNDED = Number1 * PI
+               DISPLAY "Valeur possiblement non exact"          
+               DISPLAY "Le résultat est de : " Resultat
+               STOP RUN 
+           ELSE IF SIGNE = "/" THEN
+               COMPUTE Resultat ROUNDED = Number1 / PI
+               DISPLAY "Valeur possiblement non exact"               
+               DISPLAY "Le résultat est de : " Resultat
+               STOP RUN
+           ELSE 
+               DISPLAY "Signe non reconnu"
+               STOP RUN
+           END-IF
+           STOP RUN
        END-IF.
-       DISPLAY "Entrez le premier nombre(si négatif ajouté)". 
+       DISPLAY "Entrez le premier nombre(si négatif ajouter - devant)" 
        ACCEPT Number1.
-       DISPLAY "Entrez le signe de l'opération(+, -, *, /, √)".
+       DISPLAY "Entrez le signe de l'opération(+, -, *, /)"
        ACCEPT Signe.
-       DISPLAY "Entrez le deuxieme nombre".
+       DISPLAY "Entrez le deuxieme nombre(si négatif ajouter - devant)"
        ACCEPT Number2.
        IF Signe = "+" THEN
            COMPUTE Resultat ROUNDED = Number1 + Number2
@@ -55,9 +90,9 @@
                  STOP RUN
         ELSE IF Signe = "/" THEN
            COMPUTE Resultat ROUNDED = Number1 / Number2
-           ON SIZE ERROR
-              DISPLAY "ERREUR DE DIVISION"
-              STOP RUN
+              ON SIZE ERROR
+                 DISPLAY "ERREUR DE DIVISION"
+                 STOP RUN
         ELSE
            DISPLAY "SIGNE INVALIDE"
            STOP RUN
@@ -66,3 +101,6 @@
        DISPLAY "Le résultat est de " Resultat.
        STOP RUN.
        end program Calculator.
+       *>Début le 10/09/2023
+       *>Temps estimée 8/9 Heures
+       *>Fin le 12/09/2023 21H29
